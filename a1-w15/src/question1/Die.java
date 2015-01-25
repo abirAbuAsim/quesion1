cee2984f9870b1ceb2096e29eb3f1e033b67dedf
Abir Khan <abir.sakif@gmail.com>
2015-01-25 19:14:55 +0600

    this is almost final

--- a1-w15/src/question1/Die.java
+++ a1-w15/src/question1/Die.java
@@ -1,8 +1,40 @@
 package question1;
+import java.util.Random;
 
 public class Die {
-	public static void main(String args[]){
-		System.out.println("hello'");
+	//number of sides of a dice
+	int sides;
+	
+	//the value of the side currently facing up
+	int value;
+	
+	//to randomize the sides
+	Random dice = new Random(); 
+	
+	// this Constructor wont take any argument
+	Die(){
+		sides=6;
+		roll();
+	}
+	
+	//this constructor accepts single argument
+	Die(int x){
+		sides=x;
+		roll();
+	}
+	
+	//returns the current value of the side that faces Up
+	int getValue(){
+		return  value;
+	}
+	
+	//Randomly rolls the die
+	void roll(){
+		value=1+dice.nextInt(sides);
+	}
+	
+	public String toString(){
+		return "Die [ "+sides +" sides, value=  "+getValue()+"]";
 	}
 
 }
